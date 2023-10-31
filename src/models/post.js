8bda9dbe98364172fbd448c9dbdb009852be5e15
Alex Orpi <orpi.alex@hotmail.com>
2023-10-31 17:44:51 +0100

    implemented regiter login pages and routes

--- src/models/post.js
+++ src/models/post.js
@@ -1,16 +1,19 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
+  season: {
+    type: String,
+    required: {
+      value: true,
+      message: 'Season is required',
+    },
+  },
   title: {
     type: String,
     required: {
       value: true,
       message: 'Title is required',
     },
-  },
-  slug: {
-    type: String,
-    required: true,
   },
   content: {
     type: String,
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,4 +6,30 @@ router.get('/auth/login', (req, res) => {
   res.render('auth/login');
 });
 
+router.get('/auth/register', (req, res) => {
+  res.render('auth/register');
+});
+
+router.post('/auth/register', async (req, res) => {
+  const { name, password, confirmPassword } = req.body;
+  if (password !== confirmPassword) {
+    return res.json({ message: 'Password does not match' });
+  }
+  const hashedPassword = await bcrypt.hash(password, 12);
+  await User.create({ name, email, password: hashedPassword });
+  res.status(204).render('post/userPosts');
+});
+
+router.post('/auth/login', async (req, res) => {
+  const { name, password } = req.body;
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return res.json({ message: 'Invalid credentials' });
+  }
+  await User.create({ name, email, password: hashedPassword });
+  // crear token, store in cookies etc.
+  res.status(204).render('post/userPosts');
+});
+
 export default router;
